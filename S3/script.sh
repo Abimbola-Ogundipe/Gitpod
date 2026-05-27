c2e1263ee2afd4ef2706365c45e6c2ad6132b3c0
Abimbola <ogundipeabimbola9@gmail.com>
2026-05-27 22:29:29 +0000

    updated scripts

--- S3/script.sh
+++ S3/script.sh
@@ -1,3 +1,10 @@
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+aws configure
+
 aws s3api create-bucket --bucket s3-exampple --region eu-north-1 --create-bucket-configuration LocationConstraint=eu-north-1
 aws s3 rm s3://s3-exampple  --recursive
    aws s3api delete-bucket --bucket s3-exampple
+./S3/put-bucket aws-example-gitpod 3
+ ./S3/list-bucket
